7f770e85581315173d86625587113a58835f8e90
jaredtritz <jtritz@umich.edu>
2013-06-09 16:19:58 -0400

    figured out the pattern for calling Class Based Djangotailoring View from factory view method for messages

--- views.py
+++ views.py
@@ -12,39 +12,34 @@ from djangotailoring.surveys.views import SinglePageSurveyView, SimpleSurveyView
 """
 
 def message_view(request, **kwargs):
-    return render(request, 'messages.html', {
-        "main_nav": main_nav(request.user, 'courses')
-    })
-    return HttpResponse("You're looking at the test message page")
+    mview = ECoach_Message_View.as_view()
+    return mview(
+        request, 
+        message='testing',
+        #message='widgets', 
+        t_name='mycoach/messages.html',
+        main_nav=main_nav(request.user, 'student_view')
+        )
 
+    #return render(request, 'messages.html', {
+    #    "main_nav": main_nav(request.user, 'student_view')
+    #})
+    #return HttpResponse("You're looking at the test message page")
 
 class ECoach_Message_View(TailoredDocView):
-    #m_messages = Messages()
 
     def dispatch(self, *args, **kwargs):
         # psudo constructor
         request = args[0]
-        #Log_Request(request)
+        self.template_name = kwargs['t_name']
+        self.message_document = 'Messages/' + kwargs['message'] + '.messages'
+        self.main_nav = kwargs['main_nav']
         #configure_source_data(request.user.username)
-        # load the nav object
-        #self.m_nav = Nav(request.path)
         return super(ECoach_Message_View, self).dispatch(*args, **kwargs)
 
-    @property 
-    def message_document(self): 
-        #msgid = self.m_nav.get_msgdoc()
-        return "Messages/testing.messages"
-        msg = self.m_messages.pathto(msgid)
-        # return "Surveys/testsurvey.survey"
-
-    @property 
-    def template_name(self):
-        template = 'mycoach/messages.html'
-        return template
- 
     #over ride context creation for the template
     def get_context_data(self, **kwargs):
         context = super(ECoach_Message_View, self).get_context_data(**kwargs)
-        #context["nav"] = self.m_nav
+        context["main_nav"] = self.main_nav
         return context
  
