05e67d0731f857439807ecc4a42f06d5af96a78e
Kenny Rosenberg <kenny.rosenberg@gmail.com>
2013-07-26 21:59:43 -0400

    a message iframe that rotates between message sections periodically by date

--- views.py
+++ views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render_to_response, render
 from django.conf import settings
 from djangotailoring.views import TailoredDocView
 from djangotailoring.project import getsubjectloader
+from datetime import date
+from datetime import datetime
 from mynav.nav import main_nav
 from .nav import inbox_nav
 """
@@ -43,6 +45,56 @@ class ECoach_Message_View(TailoredDocView):
         context = super(ECoach_Message_View, self).get_context_data(**kwargs)
         context["main_nav"] = self.main_nav
         context["inbox_nav"] = self.inbox_nav
+        return context
+
+def message_frame_rotating_view(request, **kwargs):
+    mview = ECoach_Rotating_Message_View.as_view()
+    # hack the default message for now
+    if kwargs['msg_id'] != '':
+        msg = kwargs['msg_id']
+    else:
+        msg = 'testing'
+    return mview(
+        request, 
+        message=msg, 
+        t_name='mycoach/messageframe_rotating.html',
+        inbox_nav=inbox_nav(request.user, msg),
+        main_nav=main_nav(request.user, 'student_view'),
+        rotate_count=kwargs['rotate_count'],
+        rotation_start_date=kwargs['rotation_start_date'],
+        rotate_frequency=kwargs['rotate_frequency']
+        )
+
+class ECoach_Rotating_Message_View(TailoredDocView):
+
+    def dispatch(self, *args, **kwargs):
+        # psudo constructor
+        request = args[0]
+        self.template_name = kwargs['t_name']
+        self.message_document = 'Messages/' + kwargs['message'] + '.messages'
+        self.inbox_nav = kwargs['inbox_nav']
+        self.main_nav = kwargs['main_nav']
+        self.rotate_count = kwargs['rotate_count']
+        self.rotate_frequency = kwargs['rotate_frequency']
+        self.rotation_start_date = kwargs['rotation_start_date']
+        #configure_source_data(request.user.username)
+        return super(ECoach_Rotating_Message_View, self).dispatch(*args, **kwargs)
+
+    #over ride context creation for the template
+    def get_context_data(self, **kwargs):
+        context = super(ECoach_Rotating_Message_View, self).get_context_data(**kwargs)
+        context["main_nav"] = self.main_nav
+        context["inbox_nav"] = self.inbox_nav
+
+        # rotating content selection
+        rotate_count = int(self.rotate_count)
+        rotate_frequency = int(self.rotate_frequency)
+        first_class_date = datetime.strptime(self.rotation_start_date, '%m%d%Y').date() 
+        content_index = ((datetime.now().date() - first_class_date).days + 1) / rotate_frequency
+        if rotate_count - content_index < 0:
+            content_index = content_index % rotate_count
+        context["rotating_id"] = "Rotating_" + str(content_index)
+
         return context
 
 def survey_preview_view(request, **kwargs):
